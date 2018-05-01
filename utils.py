df8b3f3d0f6715ac01cd0ffbd8fe1e0952e8f885
Quan Tran <anhquan0412@gmail.com>
2018-05-01 19:12:09 -0400

    tuning xgb in this shithole

--- utils.py
+++ utils.py
@@ -57,9 +57,9 @@ def merge_mean_to_val(val_df,glob_mean,prev_day):
 		print(f'Generating {new_fea} for validation set...')
 		val_df = pd.merge(val_df,mean_df,'left',on=cols)
 		val_df[new_fea].fillna(glob_mean,inplace=True)
-
-	del mean_df
-	gc.collect()
+		val_df[new_fea] = downcast_1col(val_df[new_fea])
+		del mean_df
+		gc.collect()
 
 	return val_df
 
@@ -79,7 +79,7 @@ def groupby_agg(spec,X_train,X_val=None):
 	# Perform the groupby
 	if spec['select']==None and spec['agg']=='size':
 		gp = X_train.groupby(spec['groupby']).size().reset_index().rename(columns={0:new_feature})
-	else:  
+	else:
 		gp = X_train[all_features]. \
 			groupby(spec['groupby'])[spec['select']]. \
 			agg(spec['agg']). \
@@ -88,16 +88,24 @@ def groupby_agg(spec,X_train,X_val=None):
 
 	# Merge back to X_train
 	X_train = X_train.merge(gp, on=spec['groupby'], how='left')
+	X_train[new_feature] = downcast_1col(X_train[new_feature])        
 	if not X_val is None:
 		print(f'Generating {new_feature} for validation set...')
 		X_val = X_val.merge(gp, on=spec['groupby'], how='left')
 		X_val[new_feature].fillna(0,inplace=True)
-
+		X_val[new_feature] = downcast_1col(X_val[new_feature])       
+        
 	del gp
 	gc.collect()
 
 	return X_train,X_val
 
+def downcast_1col(df):
+	if df.dtype == 'float64':
+		df = df.astype(np.float32)
+	if df.dtype == 'int64':
+		df = df.astype(np.int32)
+	return df
 def cum_count(cols,df):
 	new_fea = '_'.join(cols)+'_cumcount'
 	df[new_fea]=df.groupby(cols).cumcount()+1
@@ -109,14 +117,18 @@ def time_till_next_click(df,cols,new_fea):
 	df[new_fea].fillna(-1,inplace=True)
 	df[new_fea] = df[new_fea].astype(np.int32)
 	return df
+
 def next_prev_click(df,cols,next_fea,prev_fea):
-	df[clicktime_sec]=(df['click_time'].astype(np.int64) // (10**9)).astype(np.int32)
+	df['clicktime_sec']=(df['click_time'].astype(np.int64) // (10**9)).astype(np.int32)
 	df[next_fea] = (df.groupby(cols).clicktime_sec.shift(-1) - df.clicktime_sec).astype(np.float32)
 	#df[next_fea].fillna(-1,inplace=True)
-	df[prev_fea]=(df.click_sec - df.groupby(cols).clicktime_sec.shift(1)).astype(np.float32)
+	df[prev_fea]=(df.clicktime_sec - df.groupby(cols).clicktime_sec.shift(1)).astype(np.float32)
 	#df[prev_fea].fillna(-1,inplace=True)
 
 	df.drop('clicktime_sec',axis=1,inplace=True)
+    
+	df[next_fea] = downcast_1col(df[next_fea])
+	df[prev_fea] = downcast_1col(df[prev_fea])
 	gc.collect()
 	return df
 
@@ -180,8 +192,8 @@ def get_val_by_name(cols_to_drop,name):
 	val_df.drop(cols_to_drop,axis=1,inplace=True)
 	gc.collect()
 	return val_df,y_val
-def get_train(cols_to_drop,sz=3000000):
-	train_df=get_sample_timeseries('train_day8_3to16_FE.feather',sz)
+def get_train(cols_to_drop,filename,sz=3000000):
+	train_df=get_sample_timeseries(filename,sz)
 	y_train = train_df.is_attributed
 	train_df.drop(cols_to_drop,axis=1,inplace=True)
 	gc.collect()
